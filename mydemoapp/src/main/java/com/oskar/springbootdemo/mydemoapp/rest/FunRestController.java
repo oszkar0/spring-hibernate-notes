8f131772384012d02a4670c2dbfc106cbf2d2685
Oskar <oskszysiak@gmail.com>
2023-05-14 01:18:40 +0200

    Repair first app

--- mydemoapp/src/main/java/com/oskar/springbootdemo/mydemoapp/rest/FunRestController.java
+++ mydemoapp/src/main/java/com/oskar/springbootdemo/mydemoapp/rest/FunRestController.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //first add anotation
+//@RequestMapping("/")
 @RestController
+//@RequestMapping("/") its a default value here
 public class FunRestController {
     //expose "/" that return "hello world"
     @GetMapping("/") //means that this method will be called wvery time user comes to the main page of website for example www.mydemoapp.com/
