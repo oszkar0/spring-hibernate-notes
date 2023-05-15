4dd5e5abc2cab64c93888d6d146238f5cfdf41cb
Oskar <oskszysiak@gmail.com>
2023-05-15 17:53:35 +0200

    Spring Boot Actuator overview

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/oskar/springbootdemo/mydemoapp/MydemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskar.springbootdemo.mydemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
