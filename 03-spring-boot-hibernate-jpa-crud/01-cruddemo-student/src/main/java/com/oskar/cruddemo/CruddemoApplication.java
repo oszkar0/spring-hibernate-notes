796228fc88d6072fd1054660990ecff936357624
Oskar <oskszysiak@gmail.com>
2023-05-19 13:43:16 +0200

    Primary keys, auto increment

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/oskar/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/oskar/cruddemo/CruddemoApplication.java
@@ -17,7 +17,8 @@ public class CruddemoApplication {
 	@Bean	//from spirng boot //executed after spring beans have been loaded
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
 
@@ -30,5 +31,20 @@ public class CruddemoApplication {
 		studentDAO.save(student);
 		//display id of saved student
 		System.out.println("Saved student. Generated id: " + student.getId());
+	}
+
+	public void createMultipleStudents(StudentDAO studentDAO){
+		//create student object
+		System.out.println("Creating 3 student objects...");
+		Student student1 = new Student("Marek", "Kongo", "omarkon@g.com");
+		Student student2 = new Student("Olek", "Soran", "ols@g.com");
+		Student student3 = new Student("Bartek", "Kalon", "bkaloy@g.com");
+
+		//save the student object to db
+		System.out.println("Saving the students...");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
+		
 	}
 }
