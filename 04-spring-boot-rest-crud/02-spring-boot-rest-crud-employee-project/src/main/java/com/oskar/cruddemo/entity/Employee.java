6e7a74edff56b4a3333d15b4e5540d2fb328b9af
Oskar <oskszysiak@gmail.com>
2023-05-22 12:24:48 +0200

    Create employee entity class and configure JDBC properties

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee-project/src/main/java/com/oskar/cruddemo/entity/Employee.java
@@ -0,0 +1,72 @@
+package com.oskar.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastname, String email) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
