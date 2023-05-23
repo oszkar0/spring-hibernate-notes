5b6ab1c15c9372cd8006224c12669ced24712726
Oskar <oskszysiak@gmail.com>
2023-05-23 13:12:50 +0200

    Spring Data REST sorting and properties

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-project-with-spring-data-rest/src/main/java/com/oskar/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-project-with-spring-data-rest/src/main/java/com/oskar/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.oskar.cruddemo.dao;
 
 import com.oskar.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //that it, no need to write any code
 }
