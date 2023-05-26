c4128cab81b41f501ef7b0ab78eb38fda831728c
oszkar0 <oskszysiak@gmail.com>
2023-05-26 19:23:05 +0200

    Add query to get employees order by last name asc

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
-	
+
+    //add a method to sort by last name
+    //Spring Data JPA will parse the method name, looks for a spe
+    //ific format and pattern, create appropriate query behind the scenes
+    //luv2code.com/query-methods
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
