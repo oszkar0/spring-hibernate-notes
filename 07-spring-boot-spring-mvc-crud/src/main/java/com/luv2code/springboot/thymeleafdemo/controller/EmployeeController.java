fc8a49b0cdfcbdb77225901edb145ce31ffa1e8d
oszkar0 <oskszysiak@gmail.com>
2023-05-26 15:46:12 +0200

    Use db employees instead hardcoded ones

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,33 +15,19 @@ import java.util.List;
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
-	// load employee data
-
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	private EmployeeService employeeService;
+	@Autowired
+	public EmployeeController(EmployeeService employeeService){
+		this.employeeService = employeeService;
 	}
+
 
 	// add mapping for "/list"
 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
-
+		//get the employees from db
+		List<Employee> theEmployees = employeeService.findAll();
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
