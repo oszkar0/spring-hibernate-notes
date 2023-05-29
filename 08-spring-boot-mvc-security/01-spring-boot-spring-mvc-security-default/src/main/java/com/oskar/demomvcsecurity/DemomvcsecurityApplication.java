d991486329b832ae7afe60434f717ab900ffb2fa
oszkar0 <oskszysiak@gmail.com>
2023-05-29 23:29:19 +0200

    Initial commit for MVC Security

---
+++ 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/oskar/demomvcsecurity/DemomvcsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.oskar.demomvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomvcsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomvcsecurityApplication.class, args);
+	}
+
+}
