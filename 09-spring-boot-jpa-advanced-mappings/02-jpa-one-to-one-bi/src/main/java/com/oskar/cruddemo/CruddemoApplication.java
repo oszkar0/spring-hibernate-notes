17171a2ee292a1aa0a943cf84dd4afaf6564f753
Oskar <oskszysiak@gmail.com>
2023-06-02 22:42:47 +0200

    OneToOne bi-directional implementation

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/oskar/cruddemo/CruddemoApplication.java
@@ -0,0 +1,57 @@
+package com.oskar.cruddemo;
+
+import com.oskar.cruddemo.dao.AppDAO;
+import com.oskar.cruddemo.entity.Instructor;
+import com.oskar.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner -> {
+			//createInstructor(appDAO);
+			//findInstructorById(appDAO);
+			//deleteInstructor(appDAO);
+			findInstructorDetailById(appDAO);
+		};
+	}
+
+	private void createInstructor(AppDAO appDAO){
+		Instructor tempInstructor = new Instructor("Oskar", "Szysiak","osz@g.com" );
+		InstructorDetail tempInstructorDetail = new InstructorDetail("youtube.com/oskar", "pizza");
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor); // <- this will also save instructorDetails
+	}
+
+	private void findInstructorById(AppDAO appDAO){
+		int id = 1;
+		System.out.println("Finding instructor with id: " + id);
+		Instructor instructor = appDAO.findInstructorById(id);
+		System.out.println(instructor);
+	}
+
+	private void deleteInstructor(AppDAO appDAO){
+		int id = 1;
+		System.out.println("Deleting instructor with id: " + id);
+		appDAO.deleteInstructorById(id);
+		System.out.println("Done");
+	}
+
+	private void findInstructorDetailById(AppDAO appDAO){
+		int id = 2;
+		System.out.println("Finding instructor detail with id: " + id);
+		InstructorDetail instructorDetail = appDAO.findInstructorDetailById(id);
+		System.out.println(instructorDetail);
+	}
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/oskar/cruddemo/dao/AppDAO.java
@@ -0,0 +1,11 @@
+package com.oskar.cruddemo.dao;
+
+import com.oskar.cruddemo.entity.Instructor;
+import com.oskar.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+    Instructor findInstructorById(int id);
+    void deleteInstructorById(int id);
+    InstructorDetail findInstructorDetailById(int id);
+}
