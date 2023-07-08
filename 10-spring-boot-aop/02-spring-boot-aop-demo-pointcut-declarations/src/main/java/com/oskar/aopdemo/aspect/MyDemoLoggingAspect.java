2ef317a2252120f950d46d4e46689ff6204e6f76
oszkar0 <oskszysiak@gmail.com>
2023-07-08 20:05:54 +0200

    Using arguments inside advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/oskar/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/oskar/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,11 @@
 package com.oskar.aopdemo.aspect;
 
+import com.oskar.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +14,26 @@ import org.springframework.stereotype.Component;
 @Order(1)
 public class MyDemoLoggingAspect {
     @Before("com.oskar.aopdemo.aspect.MySharedPointcutExpressions.forDaoPackageExcludeGettersAndSetter()")
-    public void beforeAddAccountAdvice(){
-        System.out.println("\n===>Executing @Before advice on addAccount()");
+    public void beforeAddAccountAdvice(JoinPoint joinPoint){
+        //display method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        System.out.println("Method: " +  methodSignature + "\n");
+
+        //get args
+        Object[] args = joinPoint.getArgs();
+
+        for(Object o: args){
+            System.out.println(o);
+
+            if(o instanceof Account){
+                Account account = (Account) o;
+
+                System.out.println(account.getName());
+            }
+        }
+
+        System.out.println("\n===>Executing @Before advice on addAccount()\n");
     }
 }
 
